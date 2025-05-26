e2e718222a7ea42568605c64db34ddfb7786ef73
HOSUNG-07 <syg2003@naver.com>
2025-05-26 20:54:10 +0900

    hotgoal realized 기능 추가

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function displayOnScreen() {
+  const content1 = document.querySelector(".content");
+  content1.style.display = "none";
+  const contentBack = document.querySelector(".contentBack");
+  contentBack.style.display = "block";
+}
