d98f1e7e35b7bc8226f86d9af76cebb70266e28c
aeri0730 <dodl8549@gmail.com>
2025-05-28 02:44:10 +0900

    feature : 게시물 등록 및 등록된 게시물 불러와서 목록 조회 후 제목 출력 (#4)

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,4 +1,5 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-import { SUPABASE_URL, SUPABASE_KEY } from "./config.js";
+import { SUPABASE_URL, SUPABASE_KEY, PUBLIC_STORAGE_BUCKET } from "./config.js";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+export const storagebox = supabase.storage.from(PUBLIC_STORAGE_BUCKET);
